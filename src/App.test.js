ed54643b963ddafa959e1b7bbde9a0d2a87bc07a
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-04-25 11:28:51 +0900

    ・テスト関連（Jest、Enzyme）のライブラリを追加する。

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import App from './App';
 
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+});
+
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
 });
