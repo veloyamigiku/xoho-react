263dbb8c21a76c3af8fde9215364b85585924c24
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-09-22 09:41:41 +0900

    ・映画劇場アイテム本体コンポーネントの開閉ロジックを実装する。

--- src/components/movie/MovieTheaterItemContent.js
+++ src/components/movie/MovieTheaterItemContent.js
@@ -1,36 +1,55 @@
-import { forwardRef, useImperativeHandle, useState } from 'react';
+import classes from './MovieTheaterItemContent.module.css';
+import { forwardRef, useEffect, useImperativeHandle, useRef, useState } from 'react';
 import MovieTheaterScheduleDate from './MovieTheaterScheduleDate';
 import MovieTheaterScheduleScreen from './MovieTheaterScheduleScreen';
 import axios from 'axios';
 
 const MovieTheaterItemContent = (props, ref) => {
 
+  const movieTheaterItemContentRef = useRef();
+
   const [dateData, setDateData] = useState([]);
   const [screenData, setScreenData] = useState({});
+  const [screenOpen, setScreenOpen] = useState(false);
+
+  useEffect(() => {
+    if (screenOpen) {
+      movieTheaterItemContentRef.current.style.height = movieTheaterItemContentRef.current.scrollHeight + "px";
+    } else {
+      movieTheaterItemContentRef.current.style.height = 0;
+    }
+  }, [screenOpen]);
 
   useImperativeHandle(ref, () => ({
     getMovieScheduleDateScreen: () => {
-      Promise
-        .all([
-          axios
-            .get('https://wonderful-ptolemy-a2705b.netlify.app/.netlify/functions/movie_schedule_screen?year=2021&month=8&day=22&title=竜とそばかすの姫'),
-          axios
-            .get('https://wonderful-ptolemy-a2705b.netlify.app/.netlify/functions/movie_schedule_date?year=2021&month=8&day=24')
-        ])
-        .then(responses => {
-          responses.forEach(function (res) {
-            if (Array.isArray(res.data)) {
-              setDateData(res.data);
-            } else if (res.data.year) {
-              setScreenData(res.data);
-            }
+      if (screenOpen) {
+        setScreenOpen(false);
+      } else {
+        Promise
+          .all([
+            axios
+              .get('https://wonderful-ptolemy-a2705b.netlify.app/.netlify/functions/movie_schedule_screen?year=2021&month=8&day=22&title=竜とそばかすの姫'),
+            axios
+              .get('https://wonderful-ptolemy-a2705b.netlify.app/.netlify/functions/movie_schedule_date?year=2021&month=8&day=24')
+          ])
+          .then(responses => {
+            responses.forEach(function (res) {
+              if (Array.isArray(res.data)) {
+                setDateData(res.data);
+              } else if (res.data.year) {
+                setScreenData(res.data);
+              }
+            });
+            setScreenOpen(true);
           });
-        });
+      }
     }
   }));
 
   return (
-    <div>
+    <div
+      className={classes.MovieTheaterItemContent}
+      ref={movieTheaterItemContentRef}>
       <MovieTheaterScheduleDate
         data={dateData}
         onClick={() => console.log("click MovieTheaterScheduleDate.")} />
