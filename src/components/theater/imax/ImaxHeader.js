60c3534c14b4651a4b6c5483fad0e833ebbef303
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2021-11-05 09:22:51 +0900

    ・劇場（IMAX）の画像にalt属性を追加する。

--- src/components/theater/imax/ImaxHeader.js
+++ src/components/theater/imax/ImaxHeader.js
@@ -6,11 +6,11 @@ const ImaxHeader = function (props) {
     <div className={classes.ImaxHeader}>
       <div className={classes.ImaxHeaderLogo}>
         <div className={classes.ImaxHeaderLogoInner}>
-          <img className={classes.ImaxHeaderLogo} src={props.data.logo} />
+          <img className={classes.ImaxHeaderLogo} src={props.data.logo} alt="ImaxHeaderLogo" />
         </div>
       </div>
       <div>
-        <img className={classes.ImaxHeaderTitleImg} src={props.data.titleImgUrl} />
+        <img className={classes.ImaxHeaderTitleImg} src={props.data.titleImgUrl} alt="ImaxHeaderTitleImg" />
       </div>
     </div>
   );
