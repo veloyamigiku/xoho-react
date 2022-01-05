fb38127469a778f8aecf1303c174ecf4224f4490
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2022-01-06 08:50:44 +0900

    ・ルーティングのテストを修正する。本体コンポーネントの子コンポーネント（Route）のプロパティ（パスとコンポーネント）を検証する。

--- src/Contents.test.js
+++ src/Contents.test.js
@@ -1,8 +1,6 @@
-import { act } from '@testing-library/react';
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
-import { MemoryRouter } from 'react-router';
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Contents from './Contents';
 import NowPlaying from './components/now_playing/NowPlaying';
 import Top from './components/top/Top';
@@ -10,6 +8,7 @@ import ComingSoon from './components/coming_soon/ComingSoon';
 import TheaterTop from './components/theater/TheaterTop';
 import ImaxTop from './components/theater/imax/ImaxTop';
 import MovieTop from './components/movie/MovieTop';
+import { Route } from 'react-router-dom';
 
 Enzyme.configure({
   adapter: new EnzymeAdapter()
@@ -17,94 +16,19 @@ Enzyme.configure({
 
 describe('Contentsコンポーネント', () => {
 
-  it('ルーティングのテスト_' + MovieTop.name, async () => {
-    let wrapper;
-    await act(async () => {
-      wrapper = mount(
-        <MemoryRouter initialEntries={['/movie/1']}>
-          <Contents />
-        </MemoryRouter>
-      );
-    });
-    wrapper.update();
-
-    const movieTop = wrapper.find(MovieTop);
-    expect(movieTop).toHaveLength(1);
-  });
-
-  it('ルーティングのテスト_' + Top.name, async () => {
-    let wrapper; 
-    await act(async () => {
-      wrapper = mount(
-        <MemoryRouter initialEntries={['/']}>
-          <Contents />
-        </MemoryRouter>
-      );
-    });
-    wrapper.update();
-
-    const top = wrapper.find(Top);
-    expect(top).toHaveLength(1);
-  });
-
-  it('ルーティングのテスト_' + NowPlaying.name, async () => {
-    let wrapper;
-    await act(async () => {
-      wrapper = mount(
-        <MemoryRouter initialEntries={['/movie/now_playing']}>
-          <Contents />
-        </MemoryRouter>
-      );
-    });
-    wrapper.update();
-
-    const nowPlaying = wrapper.find(NowPlaying);
-    expect(nowPlaying).toHaveLength(1);
-  });
-
-  it('ルーティングのテスト_' + ComingSoon.name, async () => {
-    let wrapper;
-    await act(async () => {
-      wrapper = mount(
-        <MemoryRouter initialEntries={['/movie/coming_soon']}>
-          <Contents />
-        </MemoryRouter>
-      )
-    });
-    wrapper.update();
-
-    const comingSoon = wrapper.find(ComingSoon);
-    expect(comingSoon).toHaveLength(1);
-  });
-  
-  it('ルーティングのテスト_' + TheaterTop.name, async () => {
-    let wrapper;
-    await act(async () => {
-      wrapper = mount(
-        <MemoryRouter initialEntries={['/theater']}>
-          <Contents />
-        </MemoryRouter>
-      )
-    });
-    wrapper.update();
-
-    const theater = wrapper.find(TheaterTop);
-    expect(theater).toHaveLength(1);
-  });
-
-  it('ルーティングのテスト_' + ImaxTop.name, async () => {
-    let wrapper;
-    await act(async () => {
-      wrapper = mount(
-        <MemoryRouter initialEntries={['/theater/imax']}>
-          <Contents />
-        </MemoryRouter>
-      )
-    });
-    wrapper.update();
-
-    const imax = wrapper.find(ImaxTop);
-    expect(imax).toHaveLength(1);
+  it('ルーティングのテスト', () => {
+    const wrapper = shallow(<Contents />);
+    const pathMap = wrapper.find(Route).reduce((pathMap, route) => {
+      const routeProps = route.props();
+      pathMap[routeProps.path] = routeProps.component;
+      return pathMap;
+    }, {});
+    expect(pathMap['/']).toEqual(Top);
+    expect(pathMap['/movie/now_playing']).toEqual(NowPlaying);
+    expect(pathMap['/movie/coming_soon']).toEqual(ComingSoon);
+    expect(pathMap['/theater']).toEqual(TheaterTop);
+    expect(pathMap['/theater/imax']).toEqual(ImaxTop);
+    expect(pathMap['/movie/:id']).toEqual(MovieTop);
   });
   
 });
