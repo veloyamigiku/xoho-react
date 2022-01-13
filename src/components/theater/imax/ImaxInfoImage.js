998f6217d5f582d21d9c70055b8ba6d0c6b2865f
velo.yamigiku@gmail.com <velo.yamigiku@gmail.com>
2022-01-13 09:27:08 +0900

    ・IMAX情報（画像）コンポーネントに、スタイルを適用する。

--- src/components/theater/imax/ImaxInfoImage.js
+++ src/components/theater/imax/ImaxInfoImage.js
@@ -1,31 +1,35 @@
+import classes from './ImaxInfoImage.module.css';
+
 const ImaxInfoImage = function (props) {
   
   return (
-    <div>
-      <div>
+    <div className={classes.ImaxInfoImageWrap}>
+    <div className={classes.ImaxInfoImage}>
+      <div className={classes.ImaxInfoImageTitleWrap}>
         <img
           className="ImaxInfoImageTitle"
           src={props.data.titleImgUrl}
           alt="ImaxInfoImageTitle" />
       </div>
-      <div>
+      <div className={classes.ImaxInfoImageSubTitleWrap}>
         <img
           className="ImaxInfoImageSubTitle"
           src={props.data.subTitleImgUrl}
           alt="ImaxInfoImageSubTitle" />
       </div>
-      <div>
+      <div className={classes.ImaxInfoImageImgWrap}>
         <img
           className="ImaxInfoImageImg"
           src={props.data.imgUrl}
           alt="ImaxInfoImageImg" />
       </div>
-      <div>
+      <div className={classes.ImaxInfoImageTextWrap}>
         <img
           className="ImaxInfoImageText"
           src={props.data.textImgUrl}
           alt="ImaxInfoImageText" />
       </div>
+    </div>
     </div>
   );
 
